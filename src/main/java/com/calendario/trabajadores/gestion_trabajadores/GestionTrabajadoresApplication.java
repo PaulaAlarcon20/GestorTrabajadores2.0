e370ae64260873da7c5822ba1edc4ed82173c56a
fganuarve <fganuarve@gmail.com>
2025-02-05 18:43:50 +0100

    Initial commit

---
+++ src/main/java/com/calendario/trabajadores/gestion_trabajadores/GestionTrabajadoresApplication.java
@@ -0,0 +1,13 @@
+package com.calendario.trabajadores.gestion_trabajadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTrabajadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTrabajadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calendario/trabajadores/gestion_trabajadores/GestionTrabajadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendario.trabajadores.gestion_trabajadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTrabajadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
