40575f9e43efbf47a1f73ea7d8c8e003ba1eb14a
fganuarve <fganuarve@gmail.com>
2025-02-05 18:51:22 +0100

    Primer commit

--- src/main/java/com/calendario/trabajadores/gestion_trabajadores/GestionTrabajadoresApplication.java
+++ src/main/java/com/calendario/trabajadores/gestion_trabajadores/GestionTrabajadoresApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestionTrabajadoresApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GestionTrabajadoresApplication.class, args);
 	}
 
