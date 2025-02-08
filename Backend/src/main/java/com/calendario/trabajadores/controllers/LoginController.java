8834836feb0841453ed6c1b53846485a5706979a
fganuarve <fganuarve@gmail.com>
2025-02-08 17:59:21 +0100

    arreglado backend, conecta con la base de datos y devuelve una respuesta en postman

--- Backend/src/main/java/com/calendario/trabajadores/controllers/LoginController.java
+++ Backend/src/main/java/com/calendario/trabajadores/controllers/LoginController.java
@@ -1,44 +1,27 @@
 package com.calendario.trabajadores.controllers;
 
 import com.calendario.trabajadores.adminservice.AdminService;
+import com.calendario.trabajadores.model.login.LoginModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 //ojo: al escoger el model, el de springframework.
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class LoginController {
     @Autowired
     private AdminService adminService;
     //Endpoints
-    @GetMapping("/login")
-    public String showLoginPage(){
-        //llamada a la vista
-        return "login";
-    }
-
     @PostMapping("/login")
     //RequestParam especifica que son parámetros de la request
-    public String login(@RequestParam String username,@RequestParam String password, Model model){
-        var serviceResponse = adminService.login(username,password);
+    public String login(@RequestBody LoginModel loginModel){
+        var serviceResponse = adminService.login(loginModel.getUsername(),loginModel.getPassword());
         if(!serviceResponse){
-            model.addAttribute("error","invalid username");
             //llamada a la vista
-            return "login";
+            return "error";
         }
         //llamada a la vista
-        return "redirect:/welcome";
+        return "ok";
     }
-
-    @GetMapping("/welcome")
-    public String showWelcomePage(){
-        //llamada a la vista
-        return "welcome";
-    }
-
-
 
 }
