772620af3cdc1d12b331b9c18195ca7aefe1c0b9
oscar1234 <oscarizquierd224@gmail.com>
2025-02-17 20:22:49 +0100

    [ADD] Creación Screen formulario de alta

---
+++ Front-end/front_end_gui/lib/views/SignUp_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignupForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Formulario de alta')),
+      body: Center(
+        child: Text('Pantalla de Registro'),
+      ),
+    );
+  }
+}
