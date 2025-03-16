f7293581edca29db4f9f2c767ee42174b0af031e
oscar1234 <oscarizquierd224@gmail.com>
2025-03-16 18:11:51 +0100

    [ADD] App Bar inferior creado, listo para añadir enlace a widgets

--- Front-end/front_end_gui/lib/views/Home_screen.dart
+++ Front-end/front_end_gui/lib/views/Home_screen.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:front_end_gui/views/PersonalProfile_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return HomeScreenGo();
+  }
+}
+
+class HomeScreenGo extends StatefulWidget {
+  @override
+  _HomeScreenGoState createState() => _HomeScreenGoState();
+}
+
+class _HomeScreenGoState extends State<HomeScreenGo> {
+
+
+  int _position = 0;
+
+  final List<Widget> _screen = [
+    Center(child: Text('PÁGINA INICIO'),),
+    Center(child: Text('PÁGINA CALENDARIO'),),
+    Center(child: Text('PÁGINA VIAJES'),),
+    Center(child: PersonalprofileScreen())
+  ]; // TODO VAMOS A CAMBIARLO POR WIDGETS O LOGICA DE NAVEGACIÓN
+
+  void _itemTapped(int index){
+    setState(() {
+      _position = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Home screen') ,
-      ),
+      body: _screen[_position],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _position,
+        onTap: _itemTapped,
+        items:  [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inicio'),
+          BottomNavigationBarItem(icon: Icon(Icons.calendar_month), label: 'Turnos'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Buscar'),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline ), label: 'Perfil'), //Colors.lightBlueAccent
+        ],
+
+      )  ,
     );
   }
+
 }
